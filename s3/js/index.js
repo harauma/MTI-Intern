9467d879fb61b3819eddbb0159838768fea68384
guri3 <aloesquash.lock@gmail.com>
2018-09-19 13:05:29 +0900

    バグ修正

--- s3/js/index.js
+++ s3/js/index.js
@@ -1,17 +1,16 @@
 var vm = new Vue({
     el: "#app",
     data: {
-        // サンプルタスク
         tasks: []
     },
     computed: {
-        NecessaryTasks: function(){
-            return this.tasks.filter(function(target){
+        NecessaryTasks: function() {
+            return this.tasks.filter(function(target) {
                 return target.kind === "necessary";
             });
         },
-        OptionalTasks: function(){
-            return this.tasks.filter(function(target){
+        OptionalTasks: function() {
+            return this.tasks.filter(function(target) {
                 return target.kind === "optional";
             });
         }
@@ -21,62 +20,53 @@ var vm = new Vue({
             location.href = "./login.html";
         }
         // タスクを取りに行く
-        fetch(url + "/user/tasks?userId=" + localStorage.getItem('userId'), {
+        fetch(url + "/user/tasks?userId=" + localStorage.getItem("userId"), {
             method: "POST"
         })
             .then(function(response) {
-                if(response.status == 200){
+                if (response.status == 200) {
                     return response.json();
                 }
-                return response.json().then(function(json){
+                return response.json().then(function(json) {
                     throw new Error(json.message);
                 });
             })
-            .then(function(json){
-                console.log(json);
+            .then(function(json) {
                 vm.tasks = json.Items;
             })
-            .catch(function(err){
+            .catch(function(err) {
                 console.log(err);
             });
     },
     methods: {
-        regist:function(task){
+        regist: function(task) {
+            console.log(task);
+            task.done = !task.done;
             fetch(url + "/tasks", {
                 method: "PUT",
-                headers:new Headers({
-                    "Authorization": localStorage.getItem("mti-intern")
+                headers: new Headers({
+                    Authorization: localStorage.getItem("mti-intern")
                 }),
-                body: function(){
-                    result=JSON.stringify(task);
-                    if(task.completed){
-                        task.completed = true;
-                    }else{
-                        task.completed=false;
-                    }
-                }
+                body: JSON.stringify(task)
             })
-           .then(function (response) {
-               if (response.status == 200) {
-                   return response.json();
-               }
-               return response.json().then(function (json) {
-                   throw new Error(json.message);
-               });
-           })
-           .then(function (json) {
+                .then(function(response) {
+                    if (response.status == 200) {
+                        return response.json();
+                    }
+                    return response.json().then(function(json) {
+                        throw new Error(json.message);
+                    });
+                })
+                .then(function(json) {});
 
-           })
-
-               // divide into "add" and "required"
-               for (var i = 0; i < list.length; i++) {
-                   if (list[i].kind == "add") {
-                       vm.add.add(list[i]);
-                   } else if (list[i].kind == "required") {
-                       vm.required.add(list[i]);
-                   }
-               }
-           })
+            // divide into "add" and "required"
+            // for (var i = 0; i < list.length; i++) {
+            //     if (list[i].kind == "add") {
+            //         vm.add.add(list[i]);
+            //     } else if (list[i].kind == "required") {
+            //         vm.required.add(list[i]);
+            //     }
+            // }
         }
     }
 });
