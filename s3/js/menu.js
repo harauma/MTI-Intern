e7426c5ed64b0a9f5df3eb03fb34a53a4c28a641
losiz17 <riokam.11.12@docomo.ne.jp>
2018-09-19 16:17:29 +0900

    first commit

--- s3/js/menu.js
+++ s3/js/menu.js
@@ -1,6 +1,6 @@
 Vue.component("common-menu", {
-    props: ["current"],
-    template: `
+	props: ["current"],
+	template: `
       <div class="ui uisecondary green inverted massive menu">
         <a class="item" href="./index.html" v-bind:class="{active: current==='home'}">Home</a>
         <div class="right menu" v-if="loginStatus">
@@ -11,19 +11,19 @@ Vue.component("common-menu", {
         </div>
       </div>
     `,
-    computed: {
-        loginStatus: function() {
-            return localStorage.getItem("token");
-        }
-    },
-    methods: {
-        logout: function() {
-            localStorage.removeItem("token");
-            localStorage.removeItem("userId");
-            location.href = "./login.html";
-        },
-        login: function() {
-            location.href = "./login.html";
-        }
-    }
+	computed: {
+		loginStatus: function () {
+			return localStorage.getItem("token");
+		}
+	},
+	methods: {
+		logout: function () {
+			localStorage.removeItem("token");
+			localStorage.removeItem("userId");
+			location.href = "./login.html";
+		},
+		login: function () {
+			location.href = "./login.html";
+		}
+	}
 });
